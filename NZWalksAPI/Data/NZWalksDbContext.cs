7700872a201b4f726ed41c20eb6ecd1cb2f4dbfc
omer akkaya <omer-akkaya@outlook.com>
2023-04-23 17:10:25 +0300

    Set connectionStrings and added migrations for project

---
+++ NZWalksAPI/Data/NZWalksDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalksAPI.Domain;
+
+namespace NZWalksAPI.Data
+{
+    public class NZWalksDbContext : DbContext // That class inherit DbContext which is come entity framework package we installed
+    {
+        public NZWalksDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+
+        // This collections are represent inside our database
+        public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+    }
+}
