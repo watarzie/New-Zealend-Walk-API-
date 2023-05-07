ad84089f1690ce5b8bb57e59a7a4a9890b3425c6
omer akkaya <omer-akkaya@outlook.com>
2023-05-07 16:02:53 +0300

    added register

---
+++ NZWalksAPI/Controllers/AuthController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NZWalksAPI.Models.DTO;
+
+namespace NZWalksAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        public AuthController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        //POST:/api/Auth/Register
+        [HttpPost]
+        [Route("Register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequestDto registerRequestDto)
+        {
+            var IdentityUser = new IdentityUser
+            {
+                UserName = registerRequestDto.UserName,
+                Email = registerRequestDto.UserName
+            };
+
+            var IdentityResult = await _userManager.CreateAsync(IdentityUser, registerRequestDto.Password);
+
+            if(IdentityResult.Succeeded)
+            {
+                //Add roles to this User
+                if(registerRequestDto.Roles != null && registerRequestDto.Roles.Any())
+                {
+                    IdentityResult = await _userManager.AddToRolesAsync(IdentityUser, registerRequestDto.Roles);
+                }
+
+                if(IdentityResult.Succeeded)
+                {
+                    return Ok("User was registered! Please Login.");
+                }
+
+
+            }
+
+            return BadRequest("Something went wrong");
+        }
+    }
+}
